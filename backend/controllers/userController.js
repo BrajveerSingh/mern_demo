a93d87163ef290b331a0843c8e9198b39c4ae754
BrajveerSingh <brajveer.rana@gmail.com>
2024-05-28 19:41:04 +0530

    Completed User APIs

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -123,28 +123,61 @@ const updateUserProfile = asyncHandler(async (req, res) => {
 // @route  GET /api/users
 // @access Private/Admin
 const getUsers = asyncHandler(async (req, res) => {
-    res.send('get users');
+    const users = await User.find({});
+    res.status(200).json(users);
 });
 
 // @desc   Get user by Id
 // @route  GET /api/users/:id
 // @access Private/Admin
 const getUserById = asyncHandler(async (req, res) => {
-    res.send('get user by id');
+    const user = await User.findById(req.params.id).select('-password'); //We don't want to get password
+    if (user) {
+        res.status(200).json(user);
+    }
+    res.status(404);
+    throw new Error('User not found.');
 });
 
 // @desc   Update user
 // @route  PUT /api/users/:id
 // @access Private/Admin
 const updateUser = asyncHandler(async (req, res) => {
-    res.send('update user');
+    const user = await User.findById(req.params.id);
+
+    if(user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.name || user.email;
+        user.isAdmin = Boolean(req.body.isAdmin);
+
+        const updatedUser = await user.save();
+
+        res.status(200).json({
+            _id: updateUser._id,
+            name: updatedUser.name,
+            email: updateUser.email,
+            isAdmin: updatedUser.isAdmin
+        });
+    }
+    res.status(404);
+    throw new Error('User not found.');
 });
 
 // @desc   Delete user
 // @route  DELETE /api/users/:id
 // @access Private/Admin
 const deleteUser = asyncHandler(async (req, res) => {
-    res.send('delete user');
+    const user = await User.findById(req.params.id);
+    if (user) {
+        if(user.isAdmin){
+            res.status(404);
+            throw new Error('Admin user cannot be deleted.');
+        }
+        await User.deleteOne({ _id: user._id });
+        res.status(200).json('No content');
+    }
+    res.status(404);
+    throw new Error('User not found.');
 });
 
 
