4dcd41dd0d2575b029652d5c2a6f55eb67db8eb4
BrajveerSingh <brajveer.rana@gmail.com>
2024-05-28 17:54:55 +0530

    1. Added multer dependency for image upload 2. Setup a lcoal directory for image storage

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
+import path from 'path';
 import express, { response } from 'express';
 import dotenv from 'dotenv'
 import connectDB from './config/db.js';
 import productRoutes from '../backend/routes/productRoutes.js';
 import userRoutes from '../backend/routes/userRoutes.js';
 import orderRoutes from '../backend/routes/orderRoutes.js';
+import uploadRoutes from '../backend/routes/uploadRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import cookieParser from 'cookie-parser';
 
@@ -34,7 +36,12 @@ app.use('/api/users', userRoutes);
 
 app.use('/api/orders', orderRoutes);
 
+app.use('/api/upload', uploadRoutes);
+
 app.get('/api/config/paypal', (req,res) => res.send({ clientId: process.env.PAYPAL_CLIENT_ID}));
+
+const __dirname = path.resolve(); // set __dirname to current directory
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.use(notFound);
 app.use(errorHandler);
