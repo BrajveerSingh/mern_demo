b2a407a121d90547ec988d5f6021ccc69c1b5e5f
BrajveerSingh <brajveer.rana@gmail.com>
2024-05-29 12:49:58 +0530

    Fixed bug in user email update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -144,10 +144,9 @@ const getUserById = asyncHandler(async (req, res) => {
 // @access Private/Admin
 const updateUser = asyncHandler(async (req, res) => {
     const user = await User.findById(req.params.id);
-
     if(user) {
         user.name = req.body.name || user.name;
-        user.email = req.body.name || user.email;
+        user.email = req.body.email || user.email;
         user.isAdmin = Boolean(req.body.isAdmin);
 
         const updatedUser = await user.save();
