2d18c4b5e5d62c44fc9bb6c5d285bd606458a057
BrajveerSingh <brajveer.rana@gmail.com>
2024-05-29 16:36:55 +0530

    Paginated Home Screen and Product List Screen

--- front-end/src/screens/HomeScreen.jsx
+++ front-end/src/screens/HomeScreen.jsx
@@ -7,6 +7,7 @@ import { useGetProductsQuery } from '../slices/productsApiSlice';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 import { useParams } from 'react-router-dom';
+import Paginate from '../components/Paginate';
 
 
 const HomeScreen = () => {
@@ -56,6 +57,7 @@ const HomeScreen = () => {
                             </Col>
                         ))}
                     </Row>
+                    <Paginate pages={data.pages} page={data.page} />
                 </>
             ) }
         </>
